b0dd8656da79ebed64795a0d0fb57e3403afea63
Gustavo Hideki <gustavohideki@outlook.com>
2022-06-20 11:53:00 -0300

    Limpa array de users e tweets

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ app.use(cors());
 
 // Dados armazenados
 
-const users = [{username: "bobesponja", avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"}];
-const tweets = [{username: "bobesponja", tweet: "eu amo o hub"}];
+const users = [];
+const tweets = [];
 
 // Rotas de cadastro
 
